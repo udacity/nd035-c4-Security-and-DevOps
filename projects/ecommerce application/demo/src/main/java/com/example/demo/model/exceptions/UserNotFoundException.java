53feb02b6bcfbf066e513206d48f7f3cf129da0d
Sareeta Panda <sareetapanda@yahoo.com>
2019-06-26 18:56:27 -0700

    removing the solution from the master

--- projects/ecommerce application/demo/src/main/java/com/example/demo/model/exceptions/UserNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.model.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-	
-}
--- projects/ecommerce application/demo/src/main/java/com/example/demo/persistence/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.persistence.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.persistence.model.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-	User findByUserName(String userName);
-}
