3df32ba56a6917cd5738b6375940ac151b5c73c2
Michael Perez <perez.michael.d@gmail.com>
2021-01-22 14:55:59 -0800

    Fix import errors

--- starter_code/src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
+++ starter_code/src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
@@ -8,15 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.naming.AuthenticationException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     @Autowired
