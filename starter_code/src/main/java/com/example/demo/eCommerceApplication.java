88a3689da481fcaff27e19036626d2c867dde637
Michael Perez <perez.michael.d@gmail.com>
2021-01-23 10:17:58 -0800

    Add BCrypt password encoder bean

--- starter_code/src/main/java/com/example/demo/eCommerceApplication.java
+++ starter_code/src/main/java/com/example/demo/eCommerceApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableJpaRepositories("com.example.demo.model.persistence.repositories")
 @EntityScan("com.example.demo.model.persistence")
@@ -13,6 +15,11 @@ public class eCommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(eCommerceApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
