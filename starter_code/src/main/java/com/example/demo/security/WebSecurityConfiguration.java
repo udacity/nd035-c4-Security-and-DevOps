c7b1dadeae0fe2fac36ec6fd73bf36e91986a65f
Paul O'Hara <ohara92@hotmail.com>
2021-06-10 16:45:19 +0100

    Update local and personal remote repository for Auth branch with latest Junit and Debug classes.

--- starter_code/src/main/java/com/example/demo/security/WebSecurityConfiguration.java
+++ starter_code/src/main/java/com/example/demo/security/WebSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -31,6 +33,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
                 .addFilter(new JWTAuthenticationVerficationFilter(authenticationManager()))
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                //Changes below made following feedback on original project submission.
+                //I searched and followed advice in https://knowledge.udacity.com/questions/553529 to ensure
+                //https status code "401 Unauthorized" is returned instead of "403 Forbidden" when wrong credentials are given.
+                //So added single line of code below and requested extra imports:
+                http.exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
     }
     
     @Override
@@ -38,11 +45,21 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-    
+
+    //Changes below made following feedback on original project submission.
+    //I searched and followed advice in https://knowledge.udacity.com/questions/553529 to ensure
+    //https status code "401 Unauthorized" is returned instead of "403 Forbidden" when wrong credentials are given.
+    //So added code below and commented out original configure method:
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.parentAuthenticationManager(authenticationManagerBean())
-            .userDetailsService(userDetailsService)
-            .passwordEncoder(bCryptPasswordEncoder);
+        auth.userDetailsService(userDetailsService)
+        .passwordEncoder(bCryptPasswordEncoder);
     }
+
+    //@Override
+    //protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    //    auth.parentAuthenticationManager(authenticationManagerBean())
+    //            .userDetailsService(userDetailsService)
+    //            .passwordEncoder(bCryptPasswordEncoder);
+    //}
 }
