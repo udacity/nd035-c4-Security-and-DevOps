996d52297f3f364c87db08c684ff5603e01a6cde
Tim Grein <tim@4greins.de>
2021-06-28 10:42:17 +0200

    Add @Repository to CartRepository

--- starter_code/src/main/java/com/example/demo/model/persistence/repositories/CartRepository.java
+++ starter_code/src/main/java/com/example/demo/model/persistence/repositories/CartRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.persistence.Cart;
 import com.example.demo.model.persistence.User;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 	Cart findByUser(User user);
 }
