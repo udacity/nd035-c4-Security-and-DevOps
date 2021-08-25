cc9d93578cca9d5b76319f1dba5805c9e866ffca
rahafo <rahaf.o.omar@gmail.com>
2021-08-26 00:33:57 +0300

    add authentication layer to the application

--- starter_code/src/main/java/com/example/demo/model/persistence/User.java
+++ starter_code/src/main/java/com/example/demo/model/persistence/User.java
@@ -26,7 +26,11 @@ public class User {
 	@Column(nullable = false, unique = true)
 	@JsonProperty
 	private String username;
-	
+
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@Column(nullable = false)
+	private String password;
+
 	@OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "cart_id", referencedColumnName = "id")
 	@JsonIgnore
@@ -55,7 +59,13 @@ public class User {
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
-	
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPassword(){
+		return password;
+	}
+
 	
 }
