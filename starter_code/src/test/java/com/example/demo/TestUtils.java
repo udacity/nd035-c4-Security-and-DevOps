0b5caa36ad002732f16d53292c4b5cfb5e834772
DKKHerath <darshanakalana.h@gmail.com>
2021-11-24 19:38:06 +0530

    TestUtils.java-added

---
+++ starter_code/src/test/java/com/example/demo/TestUtils.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import java.lang.reflect.Field;
+
+public class TestUtils {
+
+    public static void injectObjects(Object target, String fieldName, Object toInject){
+
+        boolean wasPrivate = false;
+
+        try {
+            Field f = target.getClass().getDeclaredField(fieldName);
+            if (!f.canAccess(target)){
+                f.setAccessible(true);
+                wasPrivate = true;
+            }
+            f.set(target, toInject);
+            if (wasPrivate){
+                f.setAccessible(false);
+            }
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
