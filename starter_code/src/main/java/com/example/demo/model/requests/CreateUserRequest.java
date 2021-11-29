86668dc7ee5322cdd959f8452f51565a5733ced8
jempitman <80392341+jempitman@users.noreply.github.com>
2021-11-29 11:34:00 +0100

    Feat: add password + confirmation fields to CreateUserRequest

--- starter_code/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ starter_code/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -7,6 +7,28 @@ public class CreateUserRequest {
 	@JsonProperty
 	private String username;
 
+	@JsonProperty
+	private String password;
+
+	@JsonProperty
+	private String confirmPassword;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
 	public String getUsername() {
 		return username;
 	}
