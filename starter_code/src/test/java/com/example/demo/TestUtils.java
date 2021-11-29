32cf251931fa4c08d23e9011f7a5d5a5f2a3f30c
jempitman <80392341+jempitman@users.noreply.github.com>
2021-11-29 14:23:20 +0100

    Test: set up testing utilities

---
+++ starter_code/src/test/java/com/example/demo/TestUtils.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import java.lang.reflect.Field;
+
+public class TestUtils {
+
+    public static void injectObject(Object target, String fieldName, Object toInject){
+
+        boolean wasPrivate = false;
+
+        try {
+            Field field = target.getClass().getDeclaredField(fieldName);
+
+            if (field.isAccessible()){
+                field.setAccessible(true);
+                wasPrivate = true;
+            }
+            field.set(target, toInject);
+
+            if(wasPrivate){
+                field.setAccessible(false);
+            }
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
