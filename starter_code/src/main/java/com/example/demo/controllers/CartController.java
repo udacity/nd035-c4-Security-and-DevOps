70db659c1f205d57ca6ab8d49fda0d599929c4dc
jempitman <80392341+jempitman@users.noreply.github.com>
2021-12-02 13:53:41 +0100

    Feat: log cart methods

--- starter_code/src/main/java/com/example/demo/controllers/CartController.java
+++ starter_code/src/main/java/com/example/demo/controllers/CartController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controllers;
 
-import java.util.Optional;
-import java.util.stream.IntStream;
-
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.ItemRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.ModifyCartRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,17 +17,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.persistence.Cart;
-import com.example.demo.model.persistence.Item;
-import com.example.demo.model.persistence.User;
-import com.example.demo.model.persistence.repositories.CartRepository;
-import com.example.demo.model.persistence.repositories.ItemRepository;
-import com.example.demo.model.persistence.repositories.UserRepository;
-import com.example.demo.model.requests.ModifyCartRequest;
+import java.util.Optional;
+import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
+
+	private static final Logger log = LoggerFactory.getLogger(CartController.class);
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -42,6 +45,11 @@ public class CartController {
 		if(!item.isPresent()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
+
+		String logMessage = "Cart: item added to cart: " + request.getItemId() +
+				"User: Username: " + request.getUsername();
+		log.info(logMessage);
+
 		Cart cart = user.getCart();
 		IntStream.range(0, request.getQuantity())
 			.forEach(i -> cart.addItem(item.get()));
@@ -59,6 +67,11 @@ public class CartController {
 		if(!item.isPresent()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
+
+		String logMessage = "Cart: item removed from cart: " + request.getItemId() +
+				"User: Username: " + request.getUsername();
+		log.info(logMessage);
+
 		Cart cart = user.getCart();
 		IntStream.range(0, request.getQuantity())
 			.forEach(i -> cart.removeItem(item.get()));
