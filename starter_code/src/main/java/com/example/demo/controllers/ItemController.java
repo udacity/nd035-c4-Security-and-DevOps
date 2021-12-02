b68f0d91b664a76d3eaa04b9296cb10f854a3eca
jempitman <80392341+jempitman@users.noreply.github.com>
2021-12-02 13:53:53 +0100

    Feat: log item methods

--- starter_code/src/main/java/com/example/demo/controllers/ItemController.java
+++ starter_code/src/main/java/com/example/demo/controllers/ItemController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controllers;
 
-import java.util.List;
-
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.repositories.ItemRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,29 +11,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.persistence.Item;
-import com.example.demo.model.persistence.repositories.ItemRepository;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/item")
 public class ItemController {
+
+	private static final Logger log = LoggerFactory.getLogger(ItemController.class);
 
 	@Autowired
 	private ItemRepository itemRepository;
 	
 	@GetMapping
 	public ResponseEntity<List<Item>> getItems() {
+		String logMessage = "Item: all items retrieved: ";
+		log.info(logMessage);
 		return ResponseEntity.ok(itemRepository.findAll());
 	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Item> getItemById(@PathVariable Long id) {
+		Item retrievedItem = itemRepository.findById(id).orElse(null);
+		if(retrievedItem == null) {
+			return ResponseEntity.notFound().build();
+		}
+		String logMessage = "Item: item retrieved: item id: " + retrievedItem.getId();
+		log.info(logMessage);
 		return ResponseEntity.of(itemRepository.findById(id));
 	}
 	
 	@GetMapping("/name/{name}")
 	public ResponseEntity<List<Item>> getItemsByName(@PathVariable String name) {
+
 		List<Item> items = itemRepository.findByName(name);
+
+		String logMessage = "Item: item retrieved: item name: " + name;
+		log.info(logMessage);
 		return items == null || items.isEmpty() ? ResponseEntity.notFound().build()
 				: ResponseEntity.ok(items);
 			
