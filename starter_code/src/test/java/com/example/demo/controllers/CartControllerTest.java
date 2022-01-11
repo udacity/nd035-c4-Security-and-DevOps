0736a78a1837612b689b511e6fd98b51fdb9a626
rizwanr <rizwanrenesa@gmail.com>
2022-01-11 19:13:52 +0600

    added CartControllerTest and utilities class

---
+++ starter_code/src/test/java/com/example/demo/controllers/CartControllerTest.java
@@ -0,0 +1,134 @@
+package com.example.demo.controllers;
+
+import com.example.demo.TestUtils;
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.ItemRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.ModifyCartRequest;
+import com.example.demo.utils.GetItemsUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.gen5.api.Assertions.assertEquals;
+import static org.junit.gen5.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CartControllerTest {
+    public static final String USERNAME = "test";
+    private CartController cartController;
+    private CartRepository cartRepository = mock(CartRepository.class);
+    private UserRepository userRepository = mock(UserRepository.class);
+    private ItemRepository itemRepository = mock(ItemRepository.class);
+
+
+    @Before
+    public void setUp() throws NoSuchFieldException, IllegalAccessException {
+        cartController = new CartController();
+        TestUtils.injectObjects(cartController, "cartRepository", cartRepository);
+        TestUtils.injectObjects(cartController, "userRepository", userRepository);
+        TestUtils.injectObjects(cartController, "itemRepository", itemRepository);
+
+
+
+
+    }
+
+    @Test
+    public void add_to_cart(){
+
+        User user = new User();
+        user.setUsername(USERNAME);
+
+        Item item = GetItemsUtils.getItem0();
+
+        Cart cart = new Cart();
+        cart.setId(0L);
+        List<Item> itemList = new ArrayList<>();
+        itemList.add(item);
+        cart.setItems(itemList);
+        cart.setTotal(new BigDecimal("2.99"));
+        cart.setUser(user);
+        user.setCart(cart);
+
+        when(userRepository.findByUsername(USERNAME)).thenReturn(user);
+        when(itemRepository.findById(0L)).thenReturn(java.util.Optional.of(item));
+
+        ModifyCartRequest request = new ModifyCartRequest();
+        request.setItemId(0L);
+        request.setQuantity(1);
+        request.setUsername("test");
+
+        ResponseEntity<Cart> response = cartController.addTocart(request);
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        Cart retrievedCart = response.getBody();
+        assertNotNull(retrievedCart);
+        assertEquals(0L, retrievedCart.getId());
+        List<Item> items = retrievedCart.getItems();
+        assertNotNull(items);
+        Item retrievedItem = items.get(0);
+        assertEquals(2, items.size());
+        assertNotNull(retrievedItem);
+        assertEquals(item, retrievedItem);
+        assertEquals(new BigDecimal("5.98"), retrievedCart.getTotal());
+        assertEquals(user, retrievedCart.getUser());
+
+    }
+
+    @Test
+    public void remove_from_cart(){
+
+        User user = new User();
+        user.setUsername(USERNAME);
+
+        Item item = GetItemsUtils.getItem0();
+
+        Cart cart = new Cart();
+        cart.setId(0L);
+        List<Item> itemList = new ArrayList<>();
+        itemList.add(item);
+        cart.setItems(itemList);
+        cart.setTotal(new BigDecimal("2.99"));
+        cart.setUser(user);
+        user.setCart(cart);
+
+        when(userRepository.findByUsername(USERNAME)).thenReturn(user);
+        when(itemRepository.findById(0L)).thenReturn(java.util.Optional.of(item));
+
+        ModifyCartRequest request = new ModifyCartRequest();
+        request.setItemId(0L);
+        request.setQuantity(1);
+        request.setUsername("test");
+
+        ResponseEntity<Cart> response = cartController.removeFromcart(request);
+
+        assertNotNull(response);
+
+        assertEquals(200, response.getStatusCodeValue());
+        Cart retrievedCart = response.getBody();
+        assertNotNull(retrievedCart);
+
+        assertEquals(0L, retrievedCart.getId());
+        List<Item> items = retrievedCart.getItems();
+        assertEquals(0, items.size());
+//        Item retrievedItem = items.get(0);
+//        assertEquals(2, items.size());
+//        assertNotNull(retrievedItem);
+//        assertEquals(item, retrievedItem);
+//        assertEquals(new BigDecimal("5.98"), retrievedCart.getTotal());
+//        assertEquals(user, retrievedCart.getUser());
+
+
+    }
+
+}
