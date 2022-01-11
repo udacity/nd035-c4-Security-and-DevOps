388901074c17b099bb9c42d1f1d737b65e195903
rizwanr <rizwanrenesa@gmail.com>
2022-01-11 17:05:24 +0600

    added password and confirm password field in UserController, user and createUserRequest models

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,22 +1,13 @@
 package com.example.demo.controllers;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.model.persistence.Cart;
 import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -46,6 +37,9 @@ public class UserController {
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
+		if (createUserRequest.getPassword().length() <3 || !createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
+			return ResponseEntity.badRequest().build();
+		}
 		userRepository.save(user);
 		return ResponseEntity.ok(user);
 	}
--- starter_code/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ starter_code/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -6,6 +6,11 @@ public class CreateUserRequest {
 
 	@JsonProperty
 	private String username;
+	@JsonProperty
+	private String password;
+	@JsonProperty
+	private String confirmPassword;
+
 
 	public String getUsername() {
 		return username;
@@ -14,4 +19,17 @@ public class CreateUserRequest {
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
 }
