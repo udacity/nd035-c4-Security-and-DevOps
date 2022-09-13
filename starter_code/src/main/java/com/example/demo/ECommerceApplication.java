3513a39fb46a634d8ea8a37830163fde62f22f99
Erwin Palani <erwinpalani@Erwins-MacBook-Pro.local>
2022-09-13 05:39:40 -0500

    rename application and update user and user request entities for auth

--- starter_code/src/main/java/com/example/demo/ECommerceApplication.java
+++ starter_code/src/main/java/com/example/demo/ECommerceApplication.java
@@ -3,15 +3,16 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("com.example.demo.model.persistence.repositories")
 @EntityScan("com.example.demo.model.persistence")
-@SpringBootApplication
-public class SareetaApplication {
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+public class ECommerceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SareetaApplication.class, args);
+		SpringApplication.run(ECommerceApplication.class, args);
 	}
 
 }
