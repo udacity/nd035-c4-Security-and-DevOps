86a61ca9fb48a3a2b2ad42f121c7d78102f8c5f2
Sergio D. Barrera <sdbeng@gmail.com>
2024-04-12 17:15:58 -0700

    Update cartControllerTest, remove cart happy path passed

--- starter_code/src/test/java/com/example/demo/controllers/CartControllerTest.java
+++ starter_code/src/test/java/com/example/demo/controllers/CartControllerTest.java
@@ -65,4 +65,33 @@ public class CartControllerTest {
         assertEquals(200, response.getStatusCodeValue());
 
     }
+
+    @Test
+    public void remove_from_cart_happy_path() {
+
+        ModifyCartRequest modifyCartRequest = new ModifyCartRequest();
+        modifyCartRequest.setUsername("dani");
+        modifyCartRequest.setItemId(1);
+        modifyCartRequest.setQuantity(1);
+
+        User user = new User();
+        Cart cart = new Cart();
+        Item item = new Item();
+        ArrayList itemsList = new ArrayList<>();
+        cart.setItems(itemsList);
+        user.setCart(cart);
+        user.setUsername(modifyCartRequest.getUsername());
+        when(userRepository.findByUsername(modifyCartRequest.getUsername())).thenReturn(user);
+
+        item.setId(1L);
+        item.setName("Round gadget");
+        item.setPrice(BigDecimal.valueOf(10.99));
+        Optional<Item> itemOptional = Optional.of(item);
+        when(itemRepository.findById(modifyCartRequest.getItemId())).thenReturn(itemOptional);
+
+        ResponseEntity<Cart> response = cartController.removeFromcart(modifyCartRequest);
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+
+    }
 }
