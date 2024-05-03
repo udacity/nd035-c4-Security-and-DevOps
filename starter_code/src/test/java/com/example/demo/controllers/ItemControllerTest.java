2edaf529271978ec80108277c197d514bdad39f6
Sergio D. Barrera <sdbeng@gmail.com>
2024-05-03 16:16:21 -0700

    Add more test methods-coverage sdd

--- starter_code/src/test/java/com/example/demo/controllers/ItemControllerTest.java
+++ starter_code/src/test/java/com/example/demo/controllers/ItemControllerTest.java
@@ -62,4 +62,38 @@ public class ItemControllerTest {
         assertNotNull(response);
         assertEquals(200, response.getStatusCodeValue());
     }
+
+    @Test
+    public void get_by_id_error_path() {
+        Long id = 1L;
+        when(itemRepository.findById(id)).thenReturn(java.util.Optional.empty());
+
+        ResponseEntity<Item> response = itemController.getItemById(id);
+        assertNotNull(response);
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+    @Test
+    public void search_by_name_error_path() {
+        String name = "Round gadget";
+        when(itemRepository.findByName(name)).thenReturn(new ArrayList<>());
+
+        ResponseEntity response = itemController.getItemsByName(name);
+        assertNotNull(response);
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+    @Test
+    public void search_by_name_happy_path() {
+        String name = "Round gadget";
+        ArrayList<Item> items = new ArrayList<>();
+        Item item = new Item();
+        item.setName("Round gadget");
+        items.add(item);
+        when(itemRepository.findByName(name)).thenReturn(items);
+
+        ResponseEntity response = itemController.getItemsByName(name);
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+    }
 }
--- starter_code/src/test/java/com/example/demo/controllers/OrderControllerTest.java
+++ starter_code/src/test/java/com/example/demo/controllers/OrderControllerTest.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import com.example.demo.TestUtils;
 import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.Item;
 import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.UserOrder;
 import com.example.demo.model.persistence.repositories.OrderRepository;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -45,5 +47,33 @@ public class OrderControllerTest {
          ResponseEntity<UserOrder> response = orderController.submit("Beatricce");
             assertNotNull(response);
             assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    public void get_orders_for_user_happy_path() {
+        User user = new User();
+        Cart cart = new Cart();
+        user.setUsername("Beatricce");
+        cart.setId(1L);
+        ArrayList items = new ArrayList<Item>();
+        cart.setItems(items);
+        cart.setTotal(BigDecimal.valueOf(250.99));
+        user.setCart(cart);
+        when(userRepository.findByUsername("Beatricce")).thenReturn(user);
+        ResponseEntity<UserOrder> response = orderController.submit("Beatricce");
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+
+        ResponseEntity<List<UserOrder>> ordersForUserResponse = orderController.getOrdersForUser("Beatricce");
+        assertNotNull(ordersForUserResponse);
+        assertEquals(200, ordersForUserResponse.getStatusCodeValue());
+    }
+
+    @Test
+    public void get_orders_for_user_not_found() {
+        ResponseEntity<List<UserOrder>> ordersForUserResponse = orderController.getOrdersForUser("Beatricce");
+        assertNotNull(ordersForUserResponse);
+        assertEquals(404, ordersForUserResponse.getStatusCodeValue());
     }
 }
