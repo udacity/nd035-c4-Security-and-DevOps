daef6dfb371ab661f5a7f33d010172d159b392f5
Neha Verma <nehbehl@gmail.com>
2025-01-02 20:25:10 +0530

    Update Item.java

--- starter_code/src/main/java/com/example/demo/model/persistence/Item.java
+++ starter_code/src/main/java/com/example/demo/model/persistence/Item.java
@@ -2,14 +2,14 @@ package com.example.demo.model.persistence;
 
 import java.math.BigDecimal;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -90,8 +90,5 @@ public class Item {
 
 	public void setDescription(String description) {
 		this.description = description;
-	}
-	
-	
-	
+	}	
 }
