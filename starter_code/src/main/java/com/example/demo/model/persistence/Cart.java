3ec3ea898aba59254dcbe98e7c129e12d61fd049
Neha Verma <nehbehl@gmail.com>
2025-01-05 19:07:13 +0530

    Update Cart.java

--- starter_code/src/main/java/com/example/demo/model/persistence/Cart.java
+++ starter_code/src/main/java/com/example/demo/model/persistence/Cart.java
@@ -2,21 +2,18 @@ package com.example.demo.model.persistence;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "cart")
