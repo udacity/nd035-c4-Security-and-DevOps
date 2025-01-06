a0b3a6d156d3134988aea7e93da2abda95d93f2a
Neha Verma <nehbehl@gmail.com>
2025-01-06 12:05:03 +0530

    Update pom.xml (#78)
    * Update pom.xml
    
    Upgraded java and spring boot version
    
    * Update application.properties
    
    * Update Cart.java
    
    * Update Item.java
    
    * Update User.java
    
    * Update UserOrder.java

--- starter_code/src/main/java/com/example/demo/model/persistence/User.java
+++ starter_code/src/main/java/com/example/demo/model/persistence/User.java
@@ -1,17 +1,17 @@
 package com.example.demo.model.persistence;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 
 @Entity
@@ -54,8 +54,5 @@ public class User {
 
 	public void setUsername(String username) {
 		this.username = username;
-	}
-	
-	
-	
+	}	
 }
